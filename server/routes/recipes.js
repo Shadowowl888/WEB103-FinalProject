60e4b284915be84049c7f3c1e4cc5046ba1796e0
Michael Ly <108250510+MichaelLy766@users.noreply.github.com>
2024-11-14 19:29:08 -0800

    filtering endpoints working + full recipe fields returned

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import { createRecipe, getRecipes, getRecipeById, updateRecipe, deleteRecipe } from '../controllers/recipes.js';
+import { createRecipe, getRecipes, getRecipeById, getRecipesByMealType, getRecipesByCuisine, getRecipesByDietaryRestriction, getRecipesByIngredient, updateRecipe, deleteRecipe } from '../controllers/recipes.js';
 
 const router = express.Router();
 
 router.get('/', getRecipes);
 router.get('/:id', getRecipeById);
+router.get('/mealtype/:mealTypeName', getRecipesByMealType);
+router.get('/cuisine/:cuisineName', getRecipesByCuisine);
+router.get('/dietary/:dietaryName', getRecipesByDietaryRestriction);
+router.get('/ingredient/:ingredientName', getRecipesByIngredient);
 router.post('/', createRecipe);
-router.delete('/:id', deleteRecipe);
 router.patch('/:id', updateRecipe);
+router.delete('/:id', deleteRecipe);
 
 export default router;
